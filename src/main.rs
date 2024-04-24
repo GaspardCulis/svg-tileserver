e050e53b9e09c2c900ad12c82c2d476319759b2b
GaspardCulis <gasdev.lp@gmail.com>
2024-04-24 20:38:31 +0200

    feat(main): Added command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, http::StatusCode, post, web, App, HttpResponse, HttpServer, Responder};
+use clap::Parser;
 use hex_color::{Display, HexColor};
 use serde::Deserialize;
 use std::sync::RwLock;
@@ -7,6 +8,7 @@ use usvg::fontdb;
 
 struct AppState {
     tree: RwLock<usvg::Tree>, // <- Mutex is necessary to mutate safely across threads
+    tile_size: u32,
 }
 
 #[derive(Deserialize)]
@@ -72,8 +74,8 @@ async fn tile(path: web::Path<(i32, i32, i32)>, data: web::Data<AppState>) -> im
     let (z, x, y) = (z as f32, x as f32, y as f32);
     let tree = data.tree.read().unwrap();
 
-    let width = 1024f32;
-    let height = 1024f32;
+    let width = data.tile_size as f32;
+    let height = width;
     let scale = 2f32.powf(z);
     let translate_x = -width * x;
     let translate_y = -height * y;
@@ -126,25 +128,49 @@ async fn tile(path: web::Path<(i32, i32, i32)>, data: web::Data<AppState>) -> im
         .body(pixmap.encode_png().unwrap())
 }
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The path of the SVG that should be served
+    #[arg()]
+    svg_path: String,
+
+    /// The size in pixels of a PNG tile
+    #[arg(short, long, default_value_t = 256)]
+    tile_size: u32,
+
+    /// The port to start the server on
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16,
+
+    /// The size in pixels of a PNG tile
+    #[arg(short, long, default_value = "127.0.0.1")]
+    bind_address: String,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    const SVG_PATH: &str =
-        "C:/Users/a903823/OneDrive - Eviden/Documents/CODE/svguez/public/svg/elecgeo/ELECGEO.PCT.opti.svg";
+    let Args {
+        svg_path,
+        tile_size,
+        port,
+        bind_address,
+    } = Args::parse();
 
     println!("Starting server...");
     let now = Instant::now();
     let tree = {
         let mut opt = usvg::Options::default();
         // Get file's absolute directory.
-        opt.resources_dir = std::fs::canonicalize(&SVG_PATH)
+        opt.resources_dir = std::fs::canonicalize(&svg_path)
             .ok()
             .and_then(|p| p.parent().map(|p| p.to_path_buf()));
 
         let mut fontdb = fontdb::Database::new();
         fontdb.load_system_fonts();
 
-        let svg_data = std::fs::read(&SVG_PATH).unwrap();
-        println!("Parsing {}...", &SVG_PATH);
+        let svg_data = std::fs::read(&svg_path).unwrap();
+        println!("Parsing {}...", &svg_path);
         usvg::Tree::from_data(&svg_data, &opt, &fontdb).unwrap()
     };
     let elapsed = now.elapsed();
@@ -152,6 +178,7 @@ async fn main() -> std::io::Result<()> {
 
     let state = web::Data::new(AppState {
         tree: RwLock::new(tree),
+        tile_size,
     });
 
     println!("Server started!");
@@ -162,7 +189,7 @@ async fn main() -> std::io::Result<()> {
             .service(tile)
     })
     .workers(8)
-    .bind(("127.0.0.1", 8080))?
+    .bind((bind_address, port))?
     .run()
     .await
 }
